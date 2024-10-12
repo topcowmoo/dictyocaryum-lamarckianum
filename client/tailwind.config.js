d88603af1bdaa5beb444e0943734c159244e10d2
Salvatore <mammoliti.sam@gmail.com>
2024-10-12 19:42:15 -0400

    server folder set up. vite and tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,36 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    // extend: {
+    //   fontFamily: {
+    //     sans: ["Roboto", "sans-serif"],
+    //   },
+    //   colors: {
+    //     title: {
+    //       light: "#004262",
+    //       dark: "#DCF0F4",
+    //     },
+    //     bgc: {
+    //       light: "#F7F9F9",
+    //       dark: "#2C3E50",
+    //     },
+    //     alltext: {
+    //       light: "#005a85",
+    //       dark: "#d4d4d4",
+    //     },
+    //     highlight: {
+    //       light: "#CC3600",
+    //       dark: "#fbb67f",
+    //     },
+    //     focusRing: {
+    //       light: "#004262",
+    //       dark: "#FFFFFF",
+    //     },
+    //   },
+    // },
   },
   plugins: [],
-}
-
+};
