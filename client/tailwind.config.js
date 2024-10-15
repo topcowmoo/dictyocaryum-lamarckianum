3101f71b4458506ebe407e37306654e4498163c0
Salvatore <mammoliti.sam@gmail.com>
2024-10-15 15:12:49 -0400

    created button component and added light and dark color schemes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,28 +8,28 @@ export default {
       fontFamily: {
         sans: ["Roboto", "Poppins", "Segoe UI", "Helvetica Neue"],
       },
-    //   colors: {
-    //     title: {
-    //       light: "#004262",
-    //       dark: "#DCF0F4",
-    //     },
-    //     bgc: {
-    //       light: "#F7F9F9",
-    //       dark: "#2C3E50",
-    //     },
-    //     alltext: {
-    //       light: "#005a85",
-    //       dark: "#d4d4d4",
-    //     },
-    //     highlight: {
-    //       light: "#CC3600",
-    //       dark: "#fbb67f",
-    //     },
-    //     focusRing: {
-    //       light: "#004262",
-    //       dark: "#FFFFFF",
-    //     },
-    //   },
+      colors: {
+        title: {
+          light: "#1D3B77",
+          dark: "#DBE2EF",
+        },
+        bgc: {
+          light: "#F7F9F9",
+          dark: "#112D4E",
+        },
+        alltext: {
+          light: "#274468",
+          dark: "#F9F7F7",
+        },
+        highlight: {
+          light: "#FED7FE",
+          dark: "#910693",
+        },
+        // focusRing: {
+        //   light: "#004262",
+        //   dark: "#FFFFFF",
+        // },
+      },
     },
   },
   plugins: [],
